e0fe79b36dbe6b022d03adfc946e5804722b1989
KedarKK1 <officialkedark1@gmail.com>
2023-07-31 07:34:31 +0530

    :art: link sign added

--- src/components/Home Page/Archievements.js
+++ src/components/Home Page/Archievements.js
@@ -24,13 +24,13 @@ useEffect(() => {
                             <p style={{ color:textColor }}><b><h4 style={{ color:textColor }}><u>1st Runner-Up 🏆</u></h4></b></p>
                             <p style={{ color:textColor }}><>At</> - <b><i>Webster - Ignite5.0 TechFest Hackathon</i></b></p>
                             <p style={{ color:textColor }}><>Org. by</> - <b><i>NMIMS, Navi Mumbai</i></b></p>
-                            <p style={{ color:textColor }}><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1bjudMlmYM3Z5KciRKNPilyKhMpul-XDg/view?usp=sharing">Certificate link</a></h6><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1iiV_1geCyPXEO9blV_vwt4scLPNBfkGY/view">Link</a></h6></p>
+                            <p style={{ color:textColor }}><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1bjudMlmYM3Z5KciRKNPilyKhMpul-XDg/view?usp=sharing">Certificate link 🔗</a></h6><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1iiV_1geCyPXEO9blV_vwt4scLPNBfkGY/view">Link 🔗</a></h6></p>
                         </Timeline.Item>
                         <Timeline.Item color="red">
                             <p style={{ color:textColor }}><b><h4 style={{ color:textColor }}>Winner/1st Rank (Solo)🏅<u></u></h4></b></p>
                             <p style={{ color:textColor }}><>At</> - <b><i>Web-On-Eye Hackathon (offline)</i></b></p>
                             <p style={{ color:textColor }}><>Org. by</> - <b><i>APCOE, Pune</i></b></p>
-                            <p style={{ color:textColor }}><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/11jfm4UzbZlHsg5RT5umz18bHLhSYVI33/view?usp=sharing">Certificate Link</a></h6></p>
+                            <p style={{ color:textColor }}><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/11jfm4UzbZlHsg5RT5umz18bHLhSYVI33/view?usp=sharing">Certificate Link 🔗</a></h6></p>
                         </Timeline.Item>
                         <Timeline.Item color="green">
                             <p style={{ color:textColor }}><b><h4 style={{ color:textColor }}>Secured 99.21 percentile(1150 rank)🎖️ among 3,00,000 students in MHT-CET'20<u></u></h4></b></p>
@@ -38,11 +38,11 @@ useEffect(() => {
                         </Timeline.Item>
                         <Timeline.Item color="red">
                             <p style={{ color:textColor }}><b><h4 style={{ color:textColor }}>300+ DSA questions solved at Leetcode<u></u></h4></b></p>
-                            <p style={{ color:textColor }}><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://www.leetcode.com/KedarKK1/"><img style={{ marginTop: 15}} src="https://img.shields.io/badge/LeetCode-000000?style=for-the-badge&logo=LeetCode&logoColor=#d16c06" alt="leetcode_badge" /> leetcode.com/KedarKK1/</a></h6></p>
+                            <p style={{ color:textColor }}><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://www.leetcode.com/KedarKK1/"><img style={{ marginTop: 15}} src="https://img.shields.io/badge/LeetCode-000000?style=for-the-badge&logo=LeetCode&logoColor=#d16c06" alt="leetcode_badge" /> leetcode.com/KedarKK1/ 🔗</a></h6></p>
                         </Timeline.Item>
                         <Timeline.Item color="green">
                             <p style={{ color:textColor }}><b><h4 style={{ color:textColor }}>5 star ⭐ coder in Problem Solving at HackerRank (55+ Questions Solved)<u></u></h4></b></p>
-                            <p style={{ color:textColor }}><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://www.hackerrank.com/officialkedark1/"><img style={{ marginTop: 15}} src="https://img.shields.io/badge/-Hackerrank-2EC866?style=for-the-badge&logo=HackerRank&logoColor=white" alt="hackerrank_badge" />hackerrank.com/officialkedark1/</a></h6></p>
+                            <p style={{ color:textColor }}><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://www.hackerrank.com/officialkedark1/"><img style={{ marginTop: 15}} src="https://img.shields.io/badge/-Hackerrank-2EC866?style=for-the-badge&logo=HackerRank&logoColor=white" alt="hackerrank_badge" />hackerrank.com/officialkedark1/ 🔗</a></h6></p>
                         </Timeline.Item>
                     </Timeline>
                 </Col>
--- src/components/Home Page/MyProjects2.js
+++ src/components/Home Page/MyProjects2.js
@@ -45,7 +45,7 @@ const MyProjects2 = (props) => {
                             <p style={{ color:textColor }}>Build  <b><i>entire backend </i></b>for auction website for users to <b><i>post an item for auction.
                                 <br />User can increasingly bet on that item. 
                                 <br />Used, WebSockets for real-time betting & within 2 min of previous bid.</i></b></p>
-                            <p style={{ color:textColor }}><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://github.com/KedarKK1/auction_website_WTL">Link</a></h6></p>
+                            <p style={{ color:textColor }}><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://github.com/KedarKK1/auction_website_WTL">Link 🔗</a></h6></p>
                                 
                         </Timeline.Item>
                         <Timeline.Item color="red">
@@ -53,14 +53,14 @@ const MyProjects2 = (props) => {
                             <p><b><h5>{getIntoTags(["React JS", "Redux", "Ant Design", "Firebase", "Google Analytics",])}<u></u></h5></b></p>
                             {/* <p style={{ color:textColor }}><h6 style={{ color:textColor }}><b>Tech Stack used/learned -</b></h6><i>React JS, Redux, Ant Design, Firebase, Google Analytics</i></p> */}
                             <p style={{ color:textColor }}><>Features</> - <b><i>Fun page-Meme sharing(CRUD using Firebase), Redux for (dark - light mode), total visits count(stored on firebase), animate-on-scroll(AOS), contact-me form validations, </i></b>Responsive</p>
-                            <p style={{ color:textColor }}><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://github.com/KedarKK1/Netlify_Portfolio_Website/">Github link</a></h6><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://kedarkoshti.netlify.app/">Link</a></h6></p>
+                            <p style={{ color:textColor }}><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://github.com/KedarKK1/Netlify_Portfolio_Website/">Github link 🔗</a></h6><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://kedarkoshti.netlify.app/">Link 🔗</a></h6></p>
                         </Timeline.Item>
                         <Timeline.Item color="green">
                             <p style={{ color:textColor }}><b><h4 style={{ color:textColor }}>PanditMitra - Pandit Booking Website (Team)<u></u></h4></b></p>
                             <p><b><h5>{getIntoTags(["Django", "Bootstrap", "PostgreSQL"])}<u></u></h5></b></p>
                             {/* <p style={{ color:textColor }}><h6 style={{ color:textColor }}><b>Tech Stack used/learned -</b></h6><i>Django, Bootstrap</i></p> */}
                             <p style={{ color:textColor }}><h6 style={{ color:textColor }}><b>Features - Static Responsive webpages creation, Backend RESTful API, Sort, Integration, Slot booking(CRUD), email-sending, authentication, authorization</b></h6></p>
-                            <p style={{ color:textColor }}><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://github.com/KedarKK1/PBL_2_PanditMitra_22-02-2022/tree/main">Link</a></h6></p>
+                            <p style={{ color:textColor }}><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://github.com/KedarKK1/PBL_2_PanditMitra_22-02-2022/tree/main">Link 🔗</a></h6></p>
                         </Timeline.Item>
                         <Timeline.Item color="red">
                             <p style={{ color:textColor }}><b><h4 style={{ color:textColor }}>Ecommerce site(Solo project)<u></u></h4></b></p>
@@ -68,19 +68,19 @@ const MyProjects2 = (props) => {
                             {/* <p style={{ color:textColor }}><h6 style={{ color:textColor }}><b>Tech Stack used/learned -</b></h6><i>MERN(Node js, React js, MongoDB, Express), Postman, Semantic UI,</i></p> */}
                             <p style={{ color:textColor }}><h6 style={{ color:textColor }}><b>Dependencies used/learned -</b></h6><i>NodeMailer, react-router-dom, react-paginator,</i></p>
                             <p style={{ color:textColor }}><h6 style={{ color:textColor }}><b>Features - CRUD(Products, Orders) using RESTful API, Authentication with JWT, Authorization, Page-Routing, Pagination, Error-handeling</b></h6></p>
-                            <p style={{ color:textColor }}><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://github.com/KedarKK1/MERN-E-COMMERCE/tree/latestBranch/">Link</a></h6></p>
+                            <p style={{ color:textColor }}><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://github.com/KedarKK1/MERN-E-COMMERCE/tree/latestBranch/">Link 🔗</a></h6></p>
                         </Timeline.Item>
                         <Timeline.Item color="green">
                             <p style={{ color:textColor }}><b><h4 style={{ color:textColor }}>California Housing Price Prediction Model ML-WebApp (Solo project)<u></u></h4></b></p>
                             <p><b><h5>{getIntoTags(["Django", "Docker", "YAML", "Linear Regression", "Scaling", "Railways App", "Gunicorn", "Jinja2 Template"])}<u></u></h5></b></p>
                             <p style={{ color:textColor }}><h6 style={{ color:textColor }}><b>Features</b> - Scaling using <b>StandardScaler</b> then trained data, used <b>LinearRegression</b> from <b>sklearn</b>, R^2 score 0.60, <b>converted to Pickle file</b> for deployment, <b>Deployment workflow automation using .yaml file</b>, written Docker file </h6></p>
-                            <p style={{ color:textColor }}><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://github.com/KedarKK1/California-Housing-Price-Prediction-Model-ML_WebApp">Link</a></h6></p>
+                            <p style={{ color:textColor }}><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://github.com/KedarKK1/California-Housing-Price-Prediction-Model-ML_WebApp">Link 🔗</a></h6></p>
                         </Timeline.Item>
                         <Timeline.Item color="red">
                             <p style={{ color:textColor }}><b><h4 style={{ color:textColor }}>Amazon Web-Scrapper</h4></b></p>
                             <p><b><h5>{getIntoTags(["BeautifulSoup", "Selenium", "CSV Writer",])}<u></u></h5></b></p>
                             <p style={{ color:textColor }}><h6 style={{ color:textColor }}><b>Features</b> - Scraping top 20 products from amazon, then <b>converting</b> it to <b>CSV File</b>, learned about XPath for web-scraping and automation </h6></p>
-                            <p style={{ color:textColor }}><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://github.com/KedarKK1/Web-Scrapper-for-amazon">Link</a></h6></p>
+                            <p style={{ color:textColor }}><h6 style={{ color:textColor }}><a target="_blank" rel="noreferrer" href="https://github.com/KedarKK1/Web-Scrapper-for-amazon">Link 🔗</a></h6></p>
                         </Timeline.Item>
                         {/* <Timeline.Item color="green">
                             <p style={{ color:textColor }}><b><h4 style={{ color:textColor }}>B'day Reminder & Daily Task Manager (Solo)<u></u></h4></b></p>
